75eadddfed3b8aaf3af86d763b931bfe36396f9b
pyunghun <564564q@naver.com>
2024-02-26 16:27:48 +0900

    1231421

--- src/main/java/com/ohgiraffers/section02/uses2/MemberInsertManager.java
+++ src/main/java/com/ohgiraffers/section02/uses2/MemberInsertManager.java
@@ -10,6 +10,7 @@ public class MemberInsertManager {
             System.out.println(members[i].getName() + "님을 회원 등록에 성공했습니다.");
         }
         System.out.println("총 " + members.length + "명의 회원 등록에 성공했습니다.");
+        System.out.println("총 " + members.length + "명의 회원 등록에 성공했습니다.");
     }
 
 
--- src/main/java/com/ohgiraffers/section02/uses2/MemberManager.java
+++ src/main/java/com/ohgiraffers/section02/uses2/MemberManager.java
@@ -6,11 +6,11 @@ public class MemberManager {
 
         MemberDTO[] members = new MemberDTO[5];
 
-        members[0] = new MemberDTO(1, "user01", "pass01", "홍길동", 20, '남');
+        members[0] = new MemberDTO(1, "user01", "pass01", "홍길동1", 20, '남');
         members[1] = new MemberDTO(2, "user02", "pass02", "유관순", 16, '여');
         members[2] = new MemberDTO(3, "user03", "pass03", "이순신", 40, '남');
         members[3] = new MemberDTO(4, "user04", "pass04", "신사임당", 36, '여');
-        members[4] = new MemberDTO(5, "user05", "pass05", "윤봉", 22, '남');
+        members[4] = new MemberDTO(5, "user06", "pass05", "윤봉", 22, '남');
 
         MemberInsertManager memberInsertManager = new MemberInsertManager();
         memberInsertManager.insert(members);
